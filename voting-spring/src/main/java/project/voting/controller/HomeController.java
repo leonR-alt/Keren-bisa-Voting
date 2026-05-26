9828a2e9f192a00dffaf919fdd4f8837a4be6827
Leona <leonarisrumahorbo@gmail.com>
2026-05-26 15:00:57 +0700

    Add home endpoint api

--- voting-spring/src/main/java/project/voting/controller/HomeController.java
+++ voting-spring/src/main/java/project/voting/controller/HomeController.java
@@ -3,11 +3,22 @@ package project.voting.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
-        return "VoteKu Backend Running";
+    public Map<String, Object> home() {
+
+        Map<String, Object> response = new HashMap<>();
+
+        response.put("application", "VoteKu API");
+        response.put("status", "online");
+        response.put("version", "1.0");
+        response.put("message", "Backend server is running successfully");
+
+        return response;
     }
 }
